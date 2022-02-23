cfec826c03900e10cdf9409361ca737355cb0f6b
Tanatat <84885042+zerunii@users.noreply.github.com>
2022-02-23 12:07:37 +0700

    Validate Form
    Validate Form

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,14 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, ActivityIndicator, FlatList} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Badge,
+} from 'native-base';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+} from 'react-navigation-header-buttons';
 
-const DetailScreen = () => {
+import axios from 'axios';
+
+import {useFocusEffect} from '@react-navigation/native';
+
+const DetailScreen = ({navigation, route}) => {
+  const {id, title} = route.params;
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      // title:'รายละเอียดสินค้า' //set แบบ statice
+      title: title, //set แบบ dynamic
+    });
+  }, [navigation]);
+
+  const [detail, setDetail] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getData = async id => {
+    setLoading(true);
+    const res = await axios.get(
+      'https://api.codingthailand.com/api/course/' + id,
+    );
+    setDetail(res.data.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData(id);
+  }, [id]);
+
+  const _onrefresh = () => {
+    getData(id);
+  };
+
   return (
     <View>
-      <Text>DetailScreen</Text>
+      <FlatList
+        data={detail}
+        keyExtractor={(item,index) => item.ch_id.toString()}
+        onRefresh={_onrefresh}
+        refreshing={loading}
+        renderItem={({item,index}) => (
+          <ListItem thumbnail>
+            <Left>
+              <Text>{index+1}</Text>
+            </Left>
+            <Body>
+              <Text>{item.ch_title}</Text>
+              <Text note numberOfLines={1}>
+                {item.ch_detail}
+              </Text>
+            </Body>
+            <Right>
+              <Badge danger>
+                <Text>{item.ch_view}</Text>
+              </Badge>
+            </Right>
+          </ListItem>
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default DetailScreen
+export default DetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,11 +1,122 @@
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Icon,
+} from 'native-base';
+import {Formik, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const validateSchema = Yup.object().shape({
+  name: Yup.string().required("กรุณาป้อนชื่อ-สกุล"),
+  email: Yup.string()
+    .email("รูปแบบอีเมล์ไม่ถูกต้อง")
+    .required("กรุณากรอกอีเมล์ใหม่"),
+  password: Yup.string()
+    .min(3, "รหัสผ่าน 3 ตัวอักษรขึ้นไป")
+    .required("กรุณาป้อนรหัสผ่านใหม่"),
+});
 
 const RegisterScreen = () => {
   return (
-    <View>
-    <Text>RegisterScreen</Text>
-  </View>
+    <Container>
+      <Content padder>
+        <Formik
+          //ค่าเริ่มต้นของข้อมูลโดยกำหนดให้ตรงกัน backend
+          initialValues={{
+            name: "",
+            email: "",
+            password: "",
+          }}
+          validationSchema={validateSchema}
+          onSubmit={(values) => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+          {/*//errors ใช้สำหรับการตรวจสอบ state (ถ้าผู้ใช้ไม่กรอกข้อมูลจะให้ error อะไรเกิดขึ้น)*/}
+          {/* touched  เมื่อผู้ใช้ไปกดที่ name และเลื่อนเม้าส์ไปด้านนอกช่อง input โดยไม่กรอกข้อมูล*/}
+
+          {({ errors, touched, values, handleChange, handleBlur }) => (
+            <Form>
+              {/* กำหนดให้มีเส้นสีแดงถ้าผู้ใช้ไม่กรอกข้อมูลชื่อ */}
+              <Item
+                fixedLabel
+                error={errors.name && touched.name ? true : false}
+              >
+                <Label>Name</Label>
+                <Input
+                  value={values.name}
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                />
+                {errors.name && touched.name && <Icon name="close-circle" />}
+              </Item>
+              {errors.name && touched.name && (
+                <Item>
+                  <Label style={{ color: "red" }}>{errors.name}</Label>
+                </Item>
+              )}
+              <Item
+                fixedLabel
+                error={errors.email && touched.email ? true : false}
+              >
+                <Label>Email</Label>
+                <Input
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                />
+                {errors.email && touched.email && <Icon name="close-circle" />}
+              </Item>
+              {errors.email && touched.email && (
+                <Item>
+                  <Label style={{ color: "red" }}>{errors.email}</Label>
+                </Item>
+              )}
+              <Item
+                fixedLabel
+                error={errors.password && touched.password ? true : false}
+              >
+                <Label>Password</Label>
+                <Input
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                />
+                {errors.password && touched.password && (
+                  <Icon name="close-circle" />
+                )}
+              </Item>
+              {errors.password && touched.password && (
+                <Item>
+                  <Label style={{ color: "red" }}>{errors.password}</Label>
+                </Item>
+              )}
+              <Button
+                block
+                large
+                style={{ marginTop: 30, backgroundColor: "darkgray" }}
+              >
+                <Text
+                  style={{ color: "black", fontSize: 25, fontWeight: "bold" }}
+                >
+                  Register
+                </Text>
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Content>
+    </Container>
   );
 };
 
